f94128d99570d4a45261b576ec3fd3b149594140
Xavier DMello <xaviermdmello@gmail.com>
2022-05-13 14:04:02 -0400

    created preliminary UI

--- scripts/UI.py
+++ scripts/UI.py
@@ -1,13 +1,35 @@
+from cgitb import text
+from tkinter import CENTER
 import PySimpleGUI as sg
-from brownie import accounts, config, network, project
-from deploy import deploy
+from blockchain_commands import get_account
 
-# hello_psg.py
+sg.theme("LightBrown13")
+elections_col = [
+    [sg.Text("Elections")],
+    [sg.Listbox(values=[], size=(40, 20), enable_events=True)],
+]
+voting_col = [
+    [sg.Image(source="images/logo.png")],
+    [sg.HorizontalSeparator()],
+    [sg.Button("Switch Account")],
+    [sg.Text(size=(40, 17), key="-TOUT-")],
+]
+accounts_col = [
+    [sg.Text("Accounts")],
+    [sg.Listbox(values=[], size=(40, 20), enable_events=True)],
+]
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
-
+layout = [
+    [
+        sg.Column(elections_col),
+        sg.VSeparator(),
+        sg.Column(voting_col, element_justification="center"),
+        sg.VSeparator(),
+        sg.Column(accounts_col),
+    ]
+]
 # Create the window
-window = sg.Window("Demo", layout)
+window = sg.Window("Elections Canada", layout)
 
 # Create an event loop
 while True:
